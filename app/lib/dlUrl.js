fcc6e0ebe812271e65283900bb37363f611081f3
hexxman <hexmanluthor@gmail.com>
2023-07-05 00:45:18 +0800

    update 1.2

--- app/lib/dlUrl.js
+++ app/lib/dlUrl.js
@@ -2,17 +2,14 @@ import { useEffect, useState } from "react"
 import { app } from "../api/auth/[...nextauth]/route"
 import { getDownloadURL, getStorage, ref } from "firebase/storage";
 
-export const DownloadURL = () => {
-const [dl, setDl] = useState('')
 
 const storage = getStorage(app);
 const resumePath = ref(storage, 'resume/MyResume.pdf');
 
-useEffect(()=> {
-getDownloadURL(resumePath).then((data) => {
-    setDl(data);
-})
-},[])
+export const DownloadURL = () => {
+const [dl, setDl] = useState('')
+
+getDownloadURL(resumePath).then(data => setDl(data))
 
 return dl
 }
